67e12a95026c9b17782ef0a329139499f57d2fec
misanaka30 <hanadibuuuu@hotmail.co.jp>
2020-11-02 14:46:22 +0900

    ユーザービュー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   root to: "prefectures#index"
   resources :users, only: [:edit, :update]
-  resources :prefectures, only: [:index, :destroy]
+  resources :prefectures, only: [:new, :create, :destroy]
 end
