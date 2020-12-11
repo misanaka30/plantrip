75c5a0f0d551c90bbb7166343034839cda4da725
misanaka30 <hanadibuuuu@hotmail.co.jp>
2020-12-11 10:51:42 +0900

    修正

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,40 +1,43 @@
 class PlacesController < ApplicationController
  before_action :set_prefecture
+ before_action :set_places_pins, only: :index
 
  def index
-  @places = @prefecture.places.includes(:user)
-  @pins = @prefecture.pins.includes(:user)
  end
 
  def new
-  @place = Place.new
+   @place = Place.new
  end
 
  def create
-    @place = @prefecture.places.new(place_params)
-    if @place.save
-      redirect_to prefecture_places_path(@prefecture)
-    else
-       @places = @prefecture.places.includes(:user)
-       @pins = @prefecture.pins.includes(:user)
-       render :new
-    end
+   @place = @prefecture.places.new(place_params)
+   if @place.save
+    redirect_to prefecture_places_path(@prefecture)
+   else
+    set_places_pins
+    render :new
+   end
  end
  
  def destroy
-  place = @prefecture.places.find(params[:id])
-  place.destroy
-  redirect_to prefecture_places_path(@prefecture)
+   place = @prefecture.places.find(params[:id])
+   place.destroy
+   redirect_to prefecture_places_path(@prefecture)
  end
 
-  private
+ private
 
-  def place_params
-    params.require(:place).permit(:name, :url ).merge(user_id: current_user.id)
-  end
+ def place_params
+   params.require(:place).permit(:name, :url ).merge(user_id: current_user.id)
+ end
 
-  def  set_prefecture
-    @prefecture = Prefecture.find(params[:prefecture_id])
-  end
+ def  set_prefecture
+   @prefecture = Prefecture.find(params[:prefecture_id])
+ end
+
+ def set_places_pins
+   @places = @prefecture.places.includes(:user)
+   @pins = @prefecture.pins.includes(:user)
+ end
  
 end
--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,13 +1,10 @@
 class Pin < ApplicationRecord
+  belongs_to :prefecture
+  belongs_to :user
+  has_one_attached :image
+
+  validates :title_id, numericality: { other_than: 1 } 
+
   extend ActiveHash::Associations::ActiveRecordExtensions
-   belongs_to :title
-
-
- belongs_to :prefecture
- belongs_to :user
-
- has_one_attached :image
-
- validates :title_id, numericality: { other_than: 1 } 
-
+  belongs_to :title
 end
